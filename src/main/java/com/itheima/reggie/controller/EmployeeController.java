0822a7414a3cb0c3265e9f3ebd23962a08d522a5
2483428575 <2483428575@qq.com>
2022-08-13 01:18:38 +0800

    2.4 全局异常类代码开发

--- src/main/java/com/itheima/reggie/controller/EmployeeController.java
+++ src/main/java/com/itheima/reggie/controller/EmployeeController.java
@@ -25,13 +25,12 @@ public class EmployeeController {
 
     /**
      * 员工登录
-     *
      * @param request
      * @param employee
      * @return
      */
     @PostMapping("/login")
-    public R<Employee> login(HttpServletRequest request, @RequestBody Employee employee) {
+    public R<Employee> login(HttpServletRequest request,@RequestBody Employee employee){
 
         //1、将页面提交的密码password进行md5加密处理
         String password = employee.getPassword();
@@ -39,28 +38,29 @@ public class EmployeeController {
 
         //2、根据页面提交的用户名username查询数据库
         LambdaQueryWrapper<Employee> queryWrapper = new LambdaQueryWrapper<>();
-        queryWrapper.eq(Employee::getUsername, employee.getUsername());
+        queryWrapper.eq(Employee::getUsername,employee.getUsername());
         Employee emp = employeeService.getOne(queryWrapper);
 
         //3、如果没有查询到则返回登录失败结果
-        if (emp == null) {
+        if(emp == null){
             return R.error("登录失败");
         }
 
         //4、密码比对，如果不一致则返回登录失败结果
-        if (!emp.getPassword().equals(password)) {
+        if(!emp.getPassword().equals(password)){
             return R.error("登录失败");
         }
 
         //5、查看员工状态，如果为已禁用状态，则返回员工已禁用结果
-        if (emp.getStatus() == 0) {
+        if(emp.getStatus() == 0){
             return R.error("账号已禁用");
         }
 
         //6、登录成功，将员工id存入Session并返回登录成功结果
-        request.getSession().setAttribute("employee", emp.getId());
+        request.getSession().setAttribute("employee",emp.getId());
         return R.success(emp);
     }
+
     /**
      * 员工退出
      * @param request
@@ -80,7 +80,7 @@ public class EmployeeController {
      */
     @PostMapping
     public R<String> save(HttpServletRequest request,@RequestBody Employee employee){
-        log.info("新增员工，员工信息{}",employee.toString());
+        log.info("新增员工，员工信息：{}",employee.toString());
 
         //设置初始密码123456，需要进行md5加密处理
         employee.setPassword(DigestUtils.md5DigestAsHex("123456".getBytes()));
@@ -88,13 +88,14 @@ public class EmployeeController {
         employee.setCreateTime(LocalDateTime.now());
         employee.setUpdateTime(LocalDateTime.now());
 
-        //获取当前登录用户的id
+        //获得当前登录用户的id
         Long empId = (Long) request.getSession().getAttribute("employee");
 
         employee.setCreateUser(empId);
         employee.setUpdateUser(empId);
 
         employeeService.save(employee);
+
         return R.success("新增员工成功");
     }
 }
