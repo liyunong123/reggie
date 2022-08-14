e1c521b63f9c215d6e50557a3e6d7c48a1ced889
2483428575 <2483428575@qq.com>
2022-08-14 22:47:02 +0800

    2.6 禁用账号功能_代码开发（1）

--- src/main/java/com/itheima/reggie/controller/EmployeeController.java
+++ src/main/java/com/itheima/reggie/controller/EmployeeController.java
@@ -119,4 +119,19 @@ public class EmployeeController {
         return R.success(pageInfo);
 
     }
+
+    /**
+     * 根据id修改员工信息
+     * @param employee
+     * @return
+     */
+    @PutMapping
+    public R<String> update(HttpServletRequest request,@RequestBody Employee employee){
+        log.info(employee.toString());
+        Long empId = (Long) request.getSession().getAttribute("employee");
+        employee.setUpdateTime(LocalDateTime.now());
+        employee.setUpdateUser(empId);
+        employeeService.updateById(employee);
+        return R.success("员信息修改成功");
+    }
 }
