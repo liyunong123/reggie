f8573ac8babf67d258d19cf77c343ab5857dca39
2483428575 <2483428575@qq.com>
2022-08-15 16:26:32 +0800

    3.3 分页查询_代码开发

--- src/main/java/com/itheima/reggie/controller/CategoryController.java
+++ src/main/java/com/itheima/reggie/controller/CategoryController.java
@@ -1,5 +1,7 @@
 package com.itheima.reggie.controller;
 
+import com.baomidou.mybatisplus.core.conditions.query.LambdaQueryWrapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.itheima.reggie.common.R;
 import com.itheima.reggie.entity.Category;
 import com.itheima.reggie.service.CategoryService;
@@ -28,4 +30,24 @@ public class CategoryController {
         return R.success("新增分类成功");
     }
 
+    /**
+     * 分页查询
+     * @param page
+     * @param pageSize
+     * @return
+     */
+    @GetMapping("/page")
+    public R<Page> page(int page,int pageSize){
+        //分页构造器
+        Page<Category> pageInfo = new Page<>(page,pageSize);
+        //条件构造器
+        LambdaQueryWrapper<Category> queryWrapper = new LambdaQueryWrapper<>();
+        //添加排序条件，根据sort进行排序
+        queryWrapper.orderByAsc(Category::getSort);
+
+        //进行分页查询
+        categoryService.page(pageInfo,queryWrapper);
+
+        return R.success(pageInfo);
+    }
 }
