bc88a655a9ac3a195a7d07e6956ecd7594c5fffe
2483428575 <2483428575@qq.com>
2022-08-16 01:17:54 +0800

    3.7 修改分类信息_代码开发

--- src/main/java/com/itheima/reggie/controller/CategoryController.java
+++ src/main/java/com/itheima/reggie/controller/CategoryController.java
@@ -64,4 +64,16 @@ public class CategoryController {
         categoryService.remove(id);
         return R.success("分类信息删除成功！");
     }
+
+    /**
+     * 修改分类信息
+     * @param category
+     * @return
+     */
+    @PutMapping
+    public R<String> update(@RequestBody  Category category){
+       log.info("修改分类信息");
+       categoryService.updateById(category);
+        return R.success("修改分类信息成功");
+    }
 }
