602b6a992a921eb34ccfd7cd4da8dd9cc86fa8eb
2483428575 <2483428575@qq.com>
2022-08-15 15:36:39 +0800

    3.1 新增分类_类和接口代码创建

---
+++ src/main/java/com/itheima/reggie/controller/CategoryController.java
@@ -0,0 +1,14 @@
+package com.itheima.reggie.controller;
+
+import com.itheima.reggie.service.CategoryService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/Category")
+public class CategoryController {
+
+    @Autowired
+    private CategoryService categoryService;
+}
