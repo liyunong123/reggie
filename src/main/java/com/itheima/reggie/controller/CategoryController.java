8d2f4a3acc48bdf4eeb8ce29a3255824a3713313
2483428575 <2483428575@qq.com>
2022-08-15 16:13:18 +0800

    3.2 新增分类_完善代码

--- src/main/java/com/itheima/reggie/controller/CategoryController.java
+++ src/main/java/com/itheima/reggie/controller/CategoryController.java
@@ -1,14 +1,31 @@
 package com.itheima.reggie.controller;
 
+import com.itheima.reggie.common.R;
+import com.itheima.reggie.entity.Category;
 import com.itheima.reggie.service.CategoryService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+@Slf4j
 @RestController
-@RequestMapping("/Category")
+@RequestMapping("/category")
 public class CategoryController {
 
     @Autowired
     private CategoryService categoryService;
+
+    /**
+     * 新增分类
+     * @param category
+     * @return
+     */
+    @PostMapping
+    public R<String> save(@RequestBody Category category){
+
+        log.info("category:{}",category);
+        categoryService.save(category);
+        return R.success("新增分类成功");
+    }
+
 }
