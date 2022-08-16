65f2d9351ad494bf7d96b3633c81d591617fc4af
2483428575 <2483428575@qq.com>
2022-08-17 00:54:23 +0800

    4.3 新增菜品_接收页面提交的数据_代码开发

--- src/main/java/com/itheima/reggie/controller/DishController.java
+++ src/main/java/com/itheima/reggie/controller/DishController.java
@@ -1,10 +1,14 @@
 package com.itheima.reggie.controller;
 
 
+import com.itheima.reggie.common.R;
+import com.itheima.reggie.dto.DishDto;
 import com.itheima.reggie.service.DishFlavorService;
 import com.itheima.reggie.service.DishService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -22,5 +26,16 @@ public class DishController {
     @Autowired
     private DishFlavorService dishFlavorService;
 
+    /**
+     * 新增caip
+     * @param dishDto
+     * @return
+     */
+    @PostMapping
+    public R<String> save(@RequestBody DishDto dishDto){
+
+        log.info(dishDto.toString());
+        return null;
+    }
 
 }
