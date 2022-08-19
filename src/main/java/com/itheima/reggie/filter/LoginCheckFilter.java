b3282f2b8e1ded7929872a70185ef6f4b598095b
2483428575 <2483428575@qq.com>
2022-08-19 12:38:28 +0800

    6.1  移动端短信验证码登录和地址管理_代码开发

--- src/main/java/com/itheima/reggie/filter/LoginCheckFilter.java
+++ src/main/java/com/itheima/reggie/filter/LoginCheckFilter.java
@@ -36,7 +36,9 @@ public class LoginCheckFilter implements Filter {
                 "/employee/logout",
                 "/backend/**",
                 "/front/**",
-                "/common/**"
+                "/common/**",
+                "/user/sendMsg",//移动端发送短信
+                "/user/login"
 
         };
 
@@ -50,7 +52,7 @@ public class LoginCheckFilter implements Filter {
             return;
         }
 
-        //4、判断登录状态，如果已登录，则直接放行
+        //4-1、判断管理端登录状态，如果已登录，则直接放行
         if(request.getSession().getAttribute("employee") != null){
             log.info("用户已登录，用户id为：{}",request.getSession().getAttribute("employee"));
 
@@ -59,6 +61,16 @@ public class LoginCheckFilter implements Filter {
 
             long id = Thread.currentThread().getId();
             log.info("线程id为：{}",id);
+            filterChain.doFilter(request,response);
+            return;
+        }
+        //4-2、判断移动端登录状态，如果已登录，则直接放行
+        if(request.getSession().getAttribute("user") != null){
+            log.info("用户已登录，用户id为：{}",request.getSession().getAttribute("user"));
+
+            Long userId = (Long) request.getSession().getAttribute("user");
+            BaseContext.setCurrentId(userId);
+
             filterChain.doFilter(request,response);
             return;
         }
